5590c5880df7c4d2878d7e2551071ed75d8271a2
praveen4858 <praveenryali048@gmail.com>
2023-07-28 15:25:14 +0530

    first commit

---
+++ IDisplayable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_7_Assignment_18
+{
+    public interface IDisplayable
+    {
+        string Display();
+    }
+
+
+}
